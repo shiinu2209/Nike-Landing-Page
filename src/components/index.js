8621b488d27e1c67da3e1be6928e849051e96871
shiinu2209 <shiinu2209@gmail.com>
2024-06-02 02:22:59 +0530

    Initial commit

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import Button from "./Button";
+import ShoeCard from "./ShoeCard";
+import Nav from "./Nav";
+import PopularProductCard from "./PopularProductCard";
+import ServiceCard from "./ServiceCard";
+import ReviewCard from "./ReviewCard";
+export { Button, ShoeCard, Nav, PopularProductCard, ServiceCard, ReviewCard };
---
+++ src/sections/index.js
@@ -0,0 +1,18 @@
+import Hero from "./Hero";
+import Footer from "./Footer";
+import CustomerReviews from "./CustomerReviews";
+import PopularProducts from "./PopularProducts";
+import Services from "./Services";
+import Subscribe from "./Subscribe";
+import SuperQuality from "./SuperQuality";
+import SpecialOffers from "./SpecialOffers";
+export {
+  Hero,
+  Footer,
+  CustomerReviews,
+  PopularProducts,
+  Services,
+  Subscribe,
+  SuperQuality,
+  SpecialOffers,
+};
